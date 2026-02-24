89665189c5f4e365c612c60a364fd1eb3c01e63e
Aftaab Mulla <aftaabmulla228@gmail.com>
2026-02-24 15:44:06 +0530

    Initial Commit

---
+++ HealPoint/HealPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealPointApp.swift
+//  HealPoint
+//
+//  Created by Aftaab Mulla on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HealPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealPointTests/HealPointTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealPointTests.swift
+//  HealPointTests
+//
+//  Created by Aftaab Mulla on 24/02/26.
+//
+
+import Testing
+@testable import HealPoint
+
+struct HealPointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
